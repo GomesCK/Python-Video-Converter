b339c430dd704940685b383c1a4b61a3a75440f9
GomesCK <afonsogomes.3.vip@gmail.com>
2025-11-30 15:36:52 -0300

    fix:ffpeg error

--- config_ffmpeg.py
+++ config_ffmpeg.py
@@ -1,7 +1,12 @@
 import os
+import platform
 
-FFMPEG_PATH = r"C:\ffmpeg\bin\ffmpeg.exe"
-FFPROBE_PATH = r"C:\ffmpeg\bin\ffprobe.exe"
+if platform.system() == "Windows":
+    FFMPEG_PATH = r"C:\ffmpeg\bin\ffmpeg.exe"
+    FFPROBE_PATH = r"C:\ffmpeg\bin\ffprobe.exe"
+else:
+    FFMPEG_PATH = "ffmpeg"
+    FFPROBE_PATH = "ffprobe"
 
 os.environ["PATH"] = os.environ.get("PATH", "") + ";" + os.path.dirname(FFMPEG_PATH)
 os.environ["FFMPEG"] = FFMPEG_PATH
